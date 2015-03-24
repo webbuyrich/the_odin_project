4d3747a850ed68465143972320f002208fa20c4c
Richard Peterson <webbuyrich@gmail.com>
2015-03-24 14:42:22 -0500

    added first euler project

---
+++ euler project/project 1.js
@@ -0,0 +1,11 @@
+var myNumbers = function(){
+    total = 0;
+    for(i=1; i<1000;i++){
+    if((i % 3===0) || (i % 5===0)){
+      total+=i;  
+        }    
+    }
+    return total;
+};
+
+myNumbers();
