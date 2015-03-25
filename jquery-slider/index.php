a9dc3c248afda7e58580a1a69a40d266bebc24ed
Richard Peterson <webbuyrich@gmail.com>
2015-03-24 23:44:59 -0500

    added j-slider folder

---
+++ jquery-slider/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Jquery Slider</title>
+        <link rel="stylesheet" href="css/slider.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <div class="wrapper">
+        <div class="" id="slider">
+            
+            <img id="1" src="images/1.jpg" alt="" class="myImage">
+            <img id="2" src="images/3.jpg" alt="" class="myImage">
+            <img id="3" src="images/4.jpg" alt="" class="myImage">
+            <img id="4" src="images/5.jpg" alt="" class="myImage">
+
+        </div>
+
+
+        <a href="#" class="left" onclick="prev(); return false;">Previous</a>
+        <a href="#" class="right" onclick="next(); return false;">Next</a>
+        </div>
+
+<script src="js/jquery-1.11.2.min.js"></script>
+<script src="js/slider.js"></script>
+
+    </body>
+</html>
